e6f5945eeaeb43fda7d32f03a330debb801be5e3
Hervé Blondeau <herve.blondeau@protonmail.com>
2025-11-24 09:16:52 +0100

    Add and enable Catppuccin color scheme

--- init.lua
+++ init.lua
@@ -894,7 +894,23 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+
+  { -- https://github.com/catppuccin/nvim
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('catppuccin').setup {
+        flavour = 'mocha',
+      }
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
